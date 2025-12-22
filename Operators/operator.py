efa64358b8625d174e49e5eb9890d8431d0bff3d
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:13:23 +0530

    Add Python Operators

---
+++ Operators/operator.py
@@ -0,0 +1,61 @@
+# Arithmetic Operator -> +, -, *, /, %
+
+# print(10 + 20)
+# print(10 - 20)
+# print(10 * 20)
+# print(10 / 20) 
+# print(10 % 3) 
+# print(10 ** 3)   
+# print(10 // 2)   
+
+# 10, 20 -> Operands
+# +      -> Operator 
+
+
+
+
+# Assignment Operator -> =, +=, -=, *=, /=, %=
+
+# a = 20
+
+# a = a + 5
+# a += 10
+# print(a) 
+
+
+
+
+# Comparison Operator -> ==, >, <, >=, <=, !=
+
+# print(10 == 20) 
+# print(10 > 20) 
+# print(10 < 20) 
+# print(10 >= 20) 
+# print(10 <= 20) 
+# print(10 != 20)  
+
+
+
+# Logical Operator -> and, or, not
+
+# print((10 > 20) or (25 < 32)) 
+
+# print(not(10 > 20)) 
+
+
+
+# Identity Operator  -> is, is not
+
+# list1 = ["apple", "banana"]
+# list2 = ["apple", "banana"]
+
+# result = list1
+
+# print(list2 is not result)  
+
+
+
+# Membership Operator -> in, not in 
+
+fruits = ["apple", "kiwi", "mango"]
+print("apple" not in fruits) 

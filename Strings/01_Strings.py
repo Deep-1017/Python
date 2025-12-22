a556984a4545744150bb1b8737c91bdce5be69f0
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:13:41 +0530

    Add Python Strings

---
+++ Strings/01_Strings.py
@@ -0,0 +1,61 @@
+name = "john"
+role = 'developer'
+age = '23'
+
+# print(name) 
+
+# Str Concat
+# print(name + role) 
+
+
+# str = "Hel,lo, Wor,ld"
+
+# print(str.strip()) 
+
+# print(str.replace("ello", "i")) 
+
+# print(str.split(","))
+
+# a = "Hello"
+# b = "world"
+
+# c = a + " " + b
+# print(c) 
+
+# print(str[1:]) 
+
+# print(str[-4:-1]) 
+
+
+
+
+# f string
+
+# name = "john"
+# age = 23
+
+# print("Hi, My name is" + name + "I am " + age + "years old")
+
+# print(f"Hi, My na\nme is {name} & I am {age} ye\tars old.") 
+
+# print('My name is \'John\'')
+
+
+
+
+
+# Boolean -> Return true or false
+
+
+
+print(10 == 9) 
+
+print(type(10 == 9))
+
+
+if (10 > 9) :
+    print("Hello") 
+
+
+# Falsy Values -> "", [], {}, (), 0, false 
+# Truthy Values -> "Name", [10, 20], true

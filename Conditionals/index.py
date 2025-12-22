aaef3d3ba7cd416366b3141593fccf02e2d6c7db
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:12:04 +0530

    Add Python Conditionals

---
+++ Conditionals/index.py
@@ -0,0 +1,32 @@
+a = int(input("Enter a number: "))
+
+# Nested if-else statement
+if (a>7):
+    if (a < 15) :
+        print("a is between 7 and 15")
+    else:
+        print("a is greater than or equal to 15")
+else: 
+    print("a is less than or equal to 7")
+
+
+
+if(10 > 7):
+    pass
+else:
+    pass
+
+
+# age = int(input("Enter your age: "))
+
+# if (age >= 18):
+#     print("You are an adult.")
+# elif(age >= 13):
+#     print("You are a teenager.")
+# else:
+#     print("You are not an adult.")  
+#       
+
+# if age >= 20: print("You are an adult.")
+# elif age >= 13: print("You are a teenager.")  
+# else: print("You are not an adult.")   

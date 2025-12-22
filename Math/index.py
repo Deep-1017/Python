e4b00033063cac8e5c56191894c5f26fa60fac09
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:13:05 +0530

    Add Python Math

---
+++ Math/index.py
@@ -0,0 +1,23 @@
+import math as m
+
+a = m.sqrt(25) 
+b = m.ceil(4.4)
+c = m.floor(4.7)
+
+# print(m.pi)
+
+# print(b, c) 
+
+
+# x = min(5, 10, 25)
+# y = max(5, 10, 25) 
+
+# print(x)
+# print(y)
+
+
+# a = -2.45465
+# print(abs(a)) 
+
+# b = pow(3, 4)
+# print(b)

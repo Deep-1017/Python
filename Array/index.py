0715504ec70301acb0865aee05424e849ab4cfa4
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:11:38 +0530

    Add Python Array

---
+++ Array/index.py
@@ -0,0 +1,15 @@
+cars = ["Toyota", "Honda", "Ford", "BMW"]
+
+# car1 = "toyota"
+# car2 = "BMW"
+
+# print(cars[0:3]) 
+
+for i in range(len(cars)):
+    print(cars[i])
+
+
+# i = 0
+# i = 1
+# i = 2
+# i = 3

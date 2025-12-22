206f43ae7a9c3931e780dece664e78b8a6e02aa5
Deep Kansara <deepkansara1212@gmail.com>
2025-12-22 11:11:50 +0530

    Add Python Basics

---
+++ Basics/01_Basic.py
@@ -0,0 +1,82 @@
+# print("Hello world !!", end=" ")
+# print('Hello')
+
+# print(10 + 20) 
+
+# Variables - Data Container
+
+
+# Define a variable
+# name = "Data Science"
+# number = input("Enter a number: ") 
+
+"""
+Check if person is adult or not
+
+print(name) 
+print(number)  
+"""
+
+
+# Comments 
+
+"""
+line 1
+line 2
+line 3
+line 4
+"""
+
+
+
+# Variables
+
+a = 10
+# first_name = int("John") 
+
+# 10 = 10
+# print(10)
+# _na10_me1 = "kndl"
+
+# print(type(first_name)) 
+
+
+
+# Local Scope and Global Scope
+# Local Variable & Global Variable
+
+# Global Variable
+# a = 10
+
+# if(a > 5):
+#     # local variable
+#     b = 20
+#     print(b) 
+
+
+# print(b) 
+
+
+# complex number = 10 + 5j 
+
+
+# Data types
+a = 10e3
+print(a) 
+
+y = 12E4
+print(y) 
+
+b = "Hello"
+b = 'Hello'
+c = 10.5
+d = True 
+
+# List of fruits (5 elements)
+fruits = ["Apple", "Banana", "Cherry", "Mango", "Orange"]
+print("Fruits list:", fruits)
+
+age = int(input("Enter your age: ")) 
+if(age >= 18):
+    print("You are an adult")
+
---
+++ Basics/general.py
@@ -0,0 +1,7 @@
+print("This is a general text") 
+
+age = 20
+
+if(age > 18):
+    print("adult")
+
